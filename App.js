5f991522ad06ae47ef7e6d6e28c5d07a2589c88e
leonardodamata <leonardodamata@gmail.com>
2021-04-18 03:13:51 -0300

    Commit Biscoito da Sorte

---
+++ App.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native'; 
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      textoFrase: '',
+      img: require('./src/biscoito.png')
+    };
+
+ 
+    this.quebraBiscoito = this.quebraBiscoito.bind(this);
+
+    this.frases = [
+      'A vida trará coisas boas se tiveres paciência.',
+      'Demonstre amor e alegria em todas as oportunidades e verás que a paz nasce dentro de você.',
+      'Não compense na ira o que lhe falta na razão.',
+      'Defeitos e virtudes são apenas dois lados da mesma moeda.',
+      'A maior de todas as torres começa no solo.',
+      'Não há que ser forte. Há que ser flexível.',
+      'Gente todo dia arruma os cabelos, por que não o coração?',
+      'Há três coisas que jamais voltam; a flecha lançada, a palavra dita e a oportunidade perdida.'
+
+    ];
+  }
+
+  sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+  }
+
+  quebraBiscoito(){
+ 
+
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+    this.setState({
+      textoFrase: ' " '+this.frases[numeroAleatorio]+' " ',
+      img: require('./src/biscoitoAberto.png')  
+    })
+
+   
+   
+  }
+  
+   render(){
+    return(
+      <View style = {styles.container} >
+
+        <Image 
+          source={this.state.img}
+          style={styles.img}
+        />
+
+        <Text style={styles.textoFrase}>{this.state.textoFrase}</Text>
+       
+        <TouchableOpacity style={styles.botao} onPress ={this.quebraBiscoito}>
+          <View style={styles.btnArea}>
+            <Text style={styles.btnTexo}>  Quebrar Biscoito </Text>
+          </View>
+        </TouchableOpacity>
+
+      </View>
+    );
+  }
+}
+
+ 
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  img:{
+    width: 250,
+    height: 250,
+  },
+  textoFrase:{
+    fontSize: 20,
+    color: '#dd7b22',
+    margin: 30,
+    fontStyle: 'italic',
+    textAlign: 'center'
+  },
+  botao: {
+    width: 230,
+    height: 50,
+    borderWidth: 2,
+    borderColor: '#dd7b22',
+    borderRadius: 25
+  },
+  btnArea:{
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  btnTexo: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#dd7b22'
+  }
+
+});
+
+ export default App;
